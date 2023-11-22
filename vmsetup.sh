f450d5e26a08066198671cd960b680d6aff8c4c7
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2023-11-22 12:58:42 +0100

    Update vmsetup.sh

--- vmsetup.sh
+++ vmsetup.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+bash <(curl -s https://raw.githubusercontent.com/LostByteSoft/Debian-10/master/remove%20ALL%20deb%2012.0%20excep%20zzz.sh -L)
+
+
+bash <(curl -s https://raw.githubusercontent.com/LostByteSoft/Debian-10/master/remove%20zzz%20evince.sh -L)
+
+
+bash <(curl -s https://raw.githubusercontent.com/LostByteSoft/Debian-10/master/remove%20zzz%20gimp.sh -L)
+
+
+bash <(curl -s https://raw.githubusercontent.com/LostByteSoft/Debian-10/master/remove%20zzz%20libreoffice.sh -L)
+
+
+bash <(curl -s https://raw.githubusercontent.com/LostByteSoft/Debian-10/master/remove%20zzz%20rhythmbox.sh -L)
+
+
+
+ 
